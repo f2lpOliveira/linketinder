4d98deba1743c2c2c0a608a8175e12a53da5e020
f2lpOliveira <contato@fellipefreire.com.br>
2024-01-31 10:26:27 -0300

    cadastroEmpresa_1.0.1.groovy

--- src/br/com/linketinder/cadastro/Cadastro.groovy
+++ src/br/com/linketinder/cadastro/Cadastro.groovy
@@ -1,12 +1,18 @@
 package br.com.linketinder.cadastro
 
 import br.com.linketinder.candidato.Candidato
+import br.com.linketinder.empresa.Empresa
 
 class Cadastro {
 
     List<Candidato> candidatos= []
+    List<Empresa> empresas= []
 
     void cadastrarCandidato(Candidato candidato){
         candidatos.add(candidato)
+    }
+
+    void cadastrarEmpresa(Empresa empresa){
+        empresas.add(empresa)
     }
 }
