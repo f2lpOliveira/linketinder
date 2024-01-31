1b1b56d8ca7551f42c3ec8173c8f29d0a272496c
f2lpOliveira <contato@fellipefreire.com.br>
2024-01-31 16:00:44 -0300

    listarCandidatos_1.0.2.groovy

--- src/br/com/linketinder/cadastro/Cadastro.groovy
+++ src/br/com/linketinder/cadastro/Cadastro.groovy
@@ -5,7 +5,58 @@ import br.com.linketinder.empresa.Empresa
 
 class Cadastro {
 
-    List<Candidato> candidatos= []
+    List<Candidato> candidatos= [
+            new Candidato(
+            "Joao",
+            "joao@email.com",
+            "123.456.789-00",
+            30,
+            "SP",
+            "12345-678",
+            "Me formei em Engenharia de software pela Estácio.",
+            ["Java", "JavaScript", "Git"]
+    ),
+    new Candidato(
+            "Priscila",
+            "priscila@email.com",
+            "987.654.321-00",
+            25,
+            "RJ",
+            "54321-876",
+            "Sou formada em enfermagem e busco uma transição de carreira para a área de tecnologia.",
+            ["Python", "C#", "Docker"]
+    ),
+            new Candidato(
+                    "Ana",
+                    "ana@email.com",
+                    "111.222.333-44",
+                    28,
+                    "MG",
+                    "98765-432",
+                    "Atualmente estudo Análise e Desenvolvimento de Sistemas pelo IFMG e busco uma oportunidade de estágio.",
+                    ["Ruby", "React", "SQL"]
+            ),
+            new Candidato(
+                    "Carlos",
+                    "carlos@email.com",
+                    "555.666.777-88",
+                    32,
+                    "RS",
+                    "87654-321",
+                    "Sou formado em Ciência da Computação pelo IFRS e concluí uma pós em Ciber Segurança pela mesma instituição.",
+                    ["C++", "Angular", "AWS"]
+            ),
+            new Candidato(
+                    "Marina",
+                    "marina@email.com",
+                    "999.888.777-66",
+                    23,
+                    "BA",
+                    "65432-109",
+                    "Tenho interesse em atuar como desenvolvedora mobile.",
+                    ["Swift", "iOS Development", "Firebase"]
+            )
+    ]
     List<Empresa> empresas= []
 
     void cadastrarCandidato(Candidato candidato){
