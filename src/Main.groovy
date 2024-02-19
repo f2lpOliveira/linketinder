fc970fee7615d15d984d83935a6a3059aa71bff6
f2lpOliveira <contato@fellipefreire.com.br>
2024-02-19 05:41:37 -0300

    bancoDados_1.0.2.groovy

--- src/Main.groovy
+++ src/Main.groovy
@@ -2,21 +2,50 @@ import br.com.linketinder.DAO.DBOperacoes
 //import br.com.linketinder.menuIterativo.Menu
 
 DBOperacoes dbOperacoes = new DBOperacoes()
-dbOperacoes.createTable("candidatos")
 
-Map<String, Object> dados = [
-        'nome': 'Satoru',
-        'sobrenome': 'Gojo',
-        'data_de_nascimento': '1989-12-07',
-        'email': 'satoru_gojo@jujutsu.co.jp',
-        'cpf': '23456789012',
-        'pais': 'Japão',
-        'cep': '12345678',
-        'descricao_pessoal': 'Feiticeiro jujutsu de Grau Especial.',
-        'senha': 'senha123'
-]
+List<String> atributoCandidatos = Arrays.asList(
+        "empid SERIAL",
+        "nome VARCHAR(10)",
+        "sobrenome VARCHAR(10)",
+        "data_de_nascimento DATE",
+        "email VARCHAR(50)",
+        "cpf VARCHAR(11) PRIMARY KEY",
+        "pais VARCHAR(15)",
+        "cep VARCHAR(8)",
+        "descricao VARCHAR(200)",
+        "senha VARCHAR(12)"
+)
 
-dbOperacoes.insert('candidatos', dados)
+List<String> atributoEmpresas = Arrays.asList(
+        "empid SERIAL",
+        "nome VARCHAR(10)",
+        "cnpj VARCHAR(14) PRIMARY KEY",
+        "email VARCHAR(50)",
+        "pais VARCHAR(15)",
+        "cep VARCHAR(8)",
+        "descricao VARCHAR(200)",
+        "senha VARCHAR(12)"
+)
+
+List<String> atributoCompetencias = Arrays.asList(
+        "empid SERIAL",
+        "stack VARCHAR(10) PRIMARY KEY"
+)
+
+List<String> atributoVagas = Arrays.asList(
+        "empid SERIAL PRIMARY KEY",
+        "nome VARCHAR(100)",
+        "stack VARCHAR(100)",
+        "local VARCHAR(15)",
+        "descricao VARCHAR(200)"
+)
+
+dbOperacoes.createTable("candidatos", atributoCandidatos)
+dbOperacoes.createTable("empresas", atributoEmpresas)
+dbOperacoes.createTable("competencias", atributoCompetencias)
+dbOperacoes.createTable("vagas", atributoVagas)
+
+
 
 //menu = new Menu()
 //menu.exibirMenu()
