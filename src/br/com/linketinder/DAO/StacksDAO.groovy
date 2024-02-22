a258cba41050d6d73daae1bbefb8e23c22f2db08
f2lpOliveira <contato@fellipefreire.com.br>
2024-02-22 14:28:35 -0300

    bancoDados_1.0.7.groovy

--- src/br/com/linketinder/DAO/StacksDAO.groovy
+++ src/br/com/linketinder/DAO/StacksDAO.groovy
@@ -12,4 +12,24 @@ class StacksDAO {
     StacksDAO(DBOperacoes dbOperacoes) {
         this.dbOperacoes = dbOperacoes
     }
+
+    void inserirDadosNaTabelaStacks() {
+        dadosStacks.each { stack ->
+            dbOperacoes.insert("Stacks", stack)
+        }
+    }
+
+    List<Map<String, Object>> buscarStacks(String condicao, String orderBy = "empid ASC") {
+        return dbOperacoes.select("Stacks", condicao, orderBy)
+    }
+
+    void atualizarCandidato(String cnpj, Map<String, Object> novosDados) {
+        String condicao = "cnpj = '$cnpj'"
+        dbOperacoes.update("Stacks", novosDados, condicao)
+    }
+
+    void excluirCandidato(String cnpj) {
+        String condicao = "cnpj = '$cnpj'"
+        dbOperacoes.delete("Stacks", condicao)
+    }
 }
--- src/br/com/linketinder/DAO/VagasDAO.groovy
+++ src/br/com/linketinder/DAO/VagasDAO.groovy
@@ -14,4 +14,24 @@ class VagasDAO {
     VagasDAO(DBOperacoes dbOperacoes) {
         this.dbOperacoes = dbOperacoes
     }
+
+    void inserirDadosNaTabelaVagas() {
+        dadosVagas.each { vaga ->
+            dbOperacoes.insert("Vagas", vaga)
+        }
+    }
+
+    List<Map<String, Object>> buscarVagas(String condicao, String orderBy = "empid ASC") {
+        return dbOperacoes.select("Vagas", condicao, orderBy)
+    }
+
+    void atualizarCandidato(String cnpj, Map<String, Object> novosDados) {
+        String condicao = "cnpj = '$cnpj'"
+        dbOperacoes.update("Vagas", novosDados, condicao)
+    }
+
+    void excluirCandidato(String cnpj) {
+        String condicao = "cnpj = '$cnpj'"
+        dbOperacoes.delete("Vagas", condicao)
+    }
 }
