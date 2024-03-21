f3453c46738309c8af8249226cde990ca53f401d
f2lpOliveira <contato@fellipefreire.com.br>
2024-03-21 19:29:42 -0300

    refactor: Refatoração do software 1.0.2

--- backend/src/main/groovy/br/com/linketinder/menuIterativo/Menu.groovy
+++ backend/src/main/groovy/br/com/linketinder/menuIterativo/Menu.groovy
@@ -3,37 +3,42 @@ package br.com.linketinder.menuIterativo
 import br.com.linketinder.cadastro.Cadastro
 
 class Menu {
-
     def cadastro = new Cadastro()
 
-    void exibirMenu(){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+    void exibirMenu() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            boolean continuar = true
 
-        def opcao
+            while (continuar) {
 
-        do {
-            println("Escolha uma das opções a seguir:")
-            println("1. Listar Todos os Candidatos")
-            println("2. Listar Todas as Empresas")
-            println("0. Sair")
+                opcoesMenuInicial()
+                String opcao = br.readLine()
 
-            opcao = br.readLine()
-
-            switch (opcao){
-                case '1' :
-                    cadastro.listarCandidatos()
-                    break
-                case '2' :
-                    cadastro.listarEmpresas()
-                    break
-                case '0' :
-                    println("Até logo!")
-                    break
-                default:
-                    println("Opção Inválida. Tente novamente!")
-                    break
+                switch (opcao) {
+                    case '1':
+                        cadastro.listarCandidatos()
+                        break
+                    case '2':
+                        cadastro.listarEmpresas()
+                        break
+                    case '0':
+                        println("Até logo!")
+                        continuar = false
+                        break
+                    default:
+                        println("Opção Inválida. Tente novamente!")
+                        break
+                }
             }
-        } while (opcao != '0')
+        } catch (IOException e) {
+            e.printStackTrace()
+        }
+    }
+
+    void opcoesMenuInicial() {
+        println("Escolha uma das opções a seguir:")
+        println("1. Listar Todos os Candidatos")
+        println("2. Listar Todas as Empresas")
+        println("0. Sair")
     }
 }
-
