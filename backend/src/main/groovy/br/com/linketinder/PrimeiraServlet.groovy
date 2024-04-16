a618e73f851437e3b942379e24bf3aba99a1f1b4
f2lpOliveira <contato@fellipefreire.com.br>
2024-04-16 07:17:36 -0300

    refactor: Implementação annotation servlet.

--- backend/src/main/groovy/br/com/linketinder/PrimeiraServlet.groovy
+++ backend/src/main/groovy/br/com/linketinder/PrimeiraServlet.groovy
@@ -5,18 +5,32 @@ import javax.servlet.annotation.WebServlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import java.nio.charset.StandardCharsets
 
-@WebServlet(urlPatterns = '/primeiraServlet')
+@WebServlet(urlPatterns = ["/linketinder", "/teste"])
 class PrimeiraServlet extends HttpServlet{
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html")
 
-        PrintWriter writer = response.getWriter()
-        writer.println("<html>")
-        writer.println("<body>")
-        writer.println("<h1>Essa é a minha primeira servlet!</h1>")
-        writer.println("</body>")
-        writer.println("</html>")
+        String htmlContent = getHtmlContent("html/index.html")
+
+        PrintWriter out = resp.getWriter()
+        out.println(htmlContent)
+    }
+
+    private String getHtmlContent(String path) throws IOException {
+        InputStream inputStream = getServletContext().getResourceAsStream(path)
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+
+        StringBuilder content = new StringBuilder()
+        String line
+        while ((line = reader.readLine()) != null) {
+            content.append(line)
+        }
+
+        reader.close()
+        return content.toString()
     }
 }
