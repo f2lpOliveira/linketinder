dd8655ee213beb12e0b5c4cd10d089d8b8484ae1
f2lpOliveira <contato@fellipefreire.com.br>
2024-04-18 09:37:48 -0300

    feat: Implementação do endpoint /vaga/cadastrar.

--- backend/src/main/groovy/br/com/linketinder/controller/VagaController.groovy
+++ backend/src/main/groovy/br/com/linketinder/controller/VagaController.groovy
@@ -1,13 +1,41 @@
 package br.com.linketinder.controller
 
-import br.com.linketinder.dao.VagaDAO
 import br.com.linketinder.model.entity.Vaga
-import br.com.linketinder.view.InteracaoVagaView
+import br.com.linketinder.service.DataProcessorService
+import br.com.linketinder.service.VagaService
 
-class VagaController {
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-    static InteracaoVagaView vagaView = new InteracaoVagaView()
-    VagaDAO vagaDAO = new VagaDAO()
+@WebServlet(urlPatterns = ["/vaga", "/vaga/cadastrar"])
+class VagaController extends HttpServlet {
+
+    VagaService vagaService = new VagaService()
+    DataProcessorService dataProcessorService = new DataProcessorService()
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            response.setContentType("text/plain;charset=UTF-8")
+
+            StringBuilder json = dataProcessorService.readJsonRequest(request)
+
+            Vaga vaga = dataProcessorService.converterJsonToObject(json.toString(), Vaga.class)
+
+            vagaService.setVagaDAO(vaga)
+
+        }catch (Exception e) {
+            e.printStackTrace()
+        }
+    }
+
+
+
+
+
 
     void exibirFormularioCadastrarVaga() {
         vagaView.formularioCadastrarVaga()
@@ -21,7 +49,5 @@ class VagaController {
        vagaView.opcaoCadastroEmpresaVaga()
     }
 
-    void setVagaDAO(Vaga vaga) {
-        vagaDAO.dbCreate(vaga)
-    }
+
 }
